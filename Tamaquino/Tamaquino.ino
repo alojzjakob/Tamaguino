a3808dc32657b68caffc9d6f3e691d76f65fe00d
Wouter Deenik <wouterd@q42.nl>
2021-04-23 12:14:31 +0200

    More simplifications (removed menu and such)

--- Tamaquino/Tamaquino.ino
+++ Tamaquino/Tamaquino.ino
@@ -1,7 +1,8 @@
-/* Tamaguino
- by Alojz Jakob <http://jakobdesign.com>
+/* Tamaquino
+ based on Tamaguino by Alojz Jakob <http://jakobdesign.com>
+ greatly simplified by Q42 <https://www.q42.nl/>
 
- ********** TAMAGUINO ***********
+ ********** TAMAQUINO ***********
  * Tamagotchi clone for Arduino *
  ********************************
  
@@ -16,346 +17,118 @@ const int button1Pin = 9;
 const int button2Pin = 8; 
 const int button3Pin = 7; 
 
-// menus
-bool menuOpened = false;
-int menu=0;
-int subMenu=1;
-bool menuDepth=false;
-bool justOpened=false;
-#define MENUSIZE 8
-#define STRING_SIZE 11
-
-const char mainMenu[MENUSIZE][8][STRING_SIZE] PROGMEM = {
-  {"food","apple","steak","water",NULL},
-  {"game",NULL},
-  {"sleep",NULL},
-  {"clean",NULL},
-  {"doctor",NULL},
-  {"discipline",NULL},
-  {"stats","hunger","happiness","health","discipline","weight","age",NULL},
-  {"settings","sound",
-    //"something",
-    NULL
-  },
-};
-
 /* ------- PET STATS ------- */
 
-float hunger=100;
-float happiness=100;
-float health=100;
-float discipline=100;
-float weight=1;
-float age=0;
+float energy = 100;
+float happiness = 100;
 
-int action=0;
-int setting=0;
+bool dead = false;
 
-bool notification = false;
-int notificationBlink=0;
-bool dead=false;
+bool sleeping = false;
 
-bool sleeping=false;
-
-float poopometer=0;
-int poops [3] = {
-  0,0,0,
-};
+float poopometer = 0;
+int poopCoordinates[3] = { 0,0,0 };
 
 void setup() {
   pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(button3Pin, INPUT_PULLUP);
 
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
 
+  // Make sure our random function does not do the same thing every time
   randomSeed(analogRead(0));
 
   setupGraphics();
 }
 
+void loop() {
 
-
-void loop() {  
-  if(!dead){
-    /* -------- MODIFY PET STATS -------- */
-    // TODO: different gradients regarding to age
-    if(sleeping){
-      hunger-=0.00005;
-      poopometer+=0.00005;
-      if(happiness-0.0001>0){
-        happiness-=0.0001;
-      }
-      health-=0.00005+countPoops()*0.0001;
-      if(discipline-0.0001>0){
-        discipline-=0.0001;
-      }
-    }else{
-      hunger-=0.00025;
-      poopometer+=0.00025;
-      if(happiness-0.0002>0){
-        happiness-=0.0002;
-      }
-      health-=0.0001+countPoops()*0.0001;
-      if(discipline-0.0002>0){
-        discipline-=0.0002;
-      }
-      //discipline-=0.02;
-    }
-    age+=0.0000025;
-
-    if(poopometer>=10){
-      poopometer=countPoops();
-      poops[round(poopometer)]=random(20,DISPLAY_WIDTH+32);
-      poopometer=0;
-    }
-    
-    if(hunger<=20 || countPoops()>0 || happiness<=20 || health<=20){
-      notification=true;  
-    }
-    if(hunger>20 && countPoops()==0 && happiness>20 && health>20){
-      notification=false;  
-      digitalWrite(13,LOW);
-    }
-
-    if(hunger<=0 || health<=0 || happiness<=0){
-      dead=true;
-    }
-
-    resetDisplay();  
-  
-    /* ------- BUTTON PRESS ACTIONS ------- */
-    
-    /* ------- BUTTON 1 - MENU ------- */
-    if(digitalRead(button1Pin) == LOW){
-      // MENU
-    
-      if(!menuOpened){
-        menuOpened=true;
-      }else{
-        if(menuDepth){
-
-          if((const char*)pgm_read_word(&(mainMenu[menu][subMenu+1]))==NULL){
-            subMenu=1;
-          }else{
-            ++subMenu;
-          }
-          setting=100*(menu+1)+subMenu;
-        }else{
-          if(menu==MENUSIZE-1){
-            menu=0;
-          }else{
-            ++menu;
-          }
-          
-          if((const char*)pgm_read_word(&(mainMenu[menu][1]))!=NULL){
-            subMenu=1;
-            
-            justOpened=true;
-          }
-          setting=100*(menu+1)+subMenu;
-        }
-      }
-
-      delay(60);
-    }
-    /* ------- BUTTON 2 - SELECT ------- */
-    if(digitalRead(button2Pin) == LOW){
-      if(menuOpened){
-
-        if(subMenu!=1 && (const char*)pgm_read_word(&(mainMenu[menu][1][0]))!=NULL){
-          action=100*(menu+1)+subMenu; 
-        }
-        if(subMenu==1 && (const char*)pgm_read_word(&(mainMenu[menu][1][0]))==NULL){
-          action=100*(menu+1)+subMenu;
-        }
-        if(subMenu==1 && (const char*)pgm_read_word(&(mainMenu[menu][1][0]))!=NULL && menuDepth){
-          action=100*(menu+1)+subMenu; 
-        }
-        if((const char*)pgm_read_word(&(mainMenu[menu][1][0]))!=NULL){
-          setting=100*(menu+1)+subMenu;
-          menuDepth=true;
-        }
-        
-      }else{
-        action=NULL;
-        
-        menuOpened=true;
-        menuDepth=true;
-        subMenu=1;
-        menu=6;
-        action=701;
-        setting=701;
-      }
-      justOpened=false;
-
-      delay(60);
-    }
-    /* ------- BUTTON 3 - BACK ------- */
-    if(digitalRead(button3Pin) == LOW){
-      if(!menuDepth){
-        menuOpened=false;
-        menu=0;
-        setting=0;
-      }else{
-        menuDepth=false;
-        setting=100*(menu+1)+1;
-      }
-      action=NULL;
-      subMenu=1;
-
-      delay(60);
-    }
-  
-  
-  
-    /* ------- SCENERY AND WALKING ------- */
-  
-    drawSunMoon();
-  
-    drawCloud();
-
-    drawMountains();
-  
-    drawDino();
-    
-    drawGround();
-    
-    drawPoops();
-    
-    drawGrass();
-    
-    drawTrees();
-
-
-      
-    
-  
-  
-    /* ------- MENUS AND ACTIONS ------- */
-    //render menu
-    if(menuOpened){
-      drawMenu();
-    }
-  
-    //do actions
-
-    if(action>0){
-
-
-      if((action==101 || action==102 || action==103) && !sleeping && random(1,(11-round(discipline/10)))==1 ){
-        //95-100 discipline = 100% chance to feed
-        //85-95 discipline = 50% chance
-        //75-85 discipline = 33.33% chance
-        //65-75 discipline = 25% chance
-        //55-65 discipline = 20% chance
-        //45-55 discipline = 16.67% chance
-        //35-45 discipline = 14.28% chance
-        //25-35 discipline = 12.5% chance
-        //15-25 discipline = 12.5% chance
-        //5-15 discipline = 10% chance
-        //0-5 discipline = 9% chance
-
-        //animate eating
-        
-        drawEatAnimation();
-        
-        switch(action){
-          //apple
-          case 101:
-            if(hunger+10>100){
-              hunger=100;
-              weight+=0.1;
-            }else{
-              hunger+=10;
-            }
-            if(health+1<=100){
-              health+=1;
-            }
-            poopometer+=0.02;
-            break;
-          //steak
-          case 102:
-            if(hunger+20>100){
-              hunger=100;
-              weight+=0.2;
-            }else{
-              hunger+=20;
-              weight+=0.1;
-            }
-            if(health-1>0){
-              health-=1;
-            }
-            poopometer+=0.05;
-            break;
-          //water
-          case 103:
-            if(hunger+5<=100){
-              hunger+=5;
-            }
-            poopometer+=0.01;
-            break;
-
-        }
-      }
-
-      switch(action){
-        case 301:
-          //sleep
-          sleeping=!sleeping;
-
-          break;
-        case 401:
-          //bath
-          resetPoops();
-          break;
-        case 501:
-          //doctor
-          if(health<60){
-            health=100;
-            drawDoctorAnimation();
-          }
-              
-          break;
-        case 601:
-          //discipline
-          if(action==601 && !sleeping){
-            if(discipline+12<=100){
-              discipline+=12;
-            }else{
-              discipline=100;
-            }
-            if(happiness-3>0){
-              happiness-=3;
-            }
-            delay(150);
-            drawWarnAnimation();
-            
-          }
-          break;
-      }
-      action=0;
-    }
-  
-    if(notification){
-      drawNotification();
-    }
-
-    show();
-  }else{
+  if (dead) {
     drawDead();
     show();
 
     if(digitalRead(button1Pin)){
+      // Reset (low-level command to go to first line of code)
       asm volatile ("  jmp 0");
     }
+
+    return;
   }
+
+  /* -------- MODIFY PET STATS -------- */
+  if (energy > 0) {
+    energy -= sleeping ? 0.00005 : 0.00025;
+  }
+
+  if (happiness > 0) {
+    happiness -= sleeping ? 0.0002 : 0.0001;
+  }
+
+  poopometer += sleeping ? 0.00005 : 0.00025;
+
+  if(poopometer >= 100){
+    int poopNumber = countPoops();
+    poopCoordinates[round(poopNumber)]=random(20,DISPLAY_WIDTH+32);
+    poopometer=0;
+  }
+
+  if(energy <= 0 && happiness <= 0){
+    dead=true;
+  }
+  
+  /* ------- BUTTON 1 - Food ------- */
+  if(digitalRead(button1Pin) == LOW){
+    drawEatAnimation();
+
+    if (energy < 100) {
+      energy += 20;
+    }
+    
+    poopometer += 0.05;
+  }
+  /* ------- BUTTON 2 - Attention ------- */
+  if(digitalRead(button2Pin) == LOW){
+    if (happiness < 100) {
+      happiness += 15;
+    }
+  }
+  /* ------- BUTTON 3 - Cleanup ------- */
+  if(digitalRead(button3Pin) == LOW){
+    resetPoops();
+  }
+
+  resetDisplay();
+
+  drawSunMoon();
+
+  drawCloud();
+
+  drawMountains();
+
+  drawDino();
+  
+  drawGround();
+  
+  drawPoops();
+  
+  drawGrass();
+  
+  drawTrees();
+
+  if(energy <= 20 || countPoops() > 0 || happiness <= 20){
+    drawNotification();
+  } else {
+    digitalWrite(13, LOW);
+  }
+
+  show();
 }
 
 int countPoops(){
   int poopsCnt = 0;
   for(int i=0; i<3; i++){
-    if(poops[i]>0){
+    if(poopCoordinates[i]>0){
       ++poopsCnt;
     }
   }
@@ -364,6 +137,6 @@ int countPoops(){
 
 void resetPoops(){
   for(int i=0; i<3; i++){
-    poops[i]=0;
+    poopCoordinates[i]=0;
   }
 }
--- Tamaquino/_graphics.ino
+++ Tamaquino/_graphics.ino
@@ -4,24 +4,30 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
+// walking
 int walkPos=0;
 int walkXPos=0;
 bool walkAnimReverse=false;
 bool walkRight=false;
 int walkDirOffset=2;
 
-//ground
+// ground
 int grassXPos=0;
 float treesXPos=-20;
-//sky
+
+// sky
 float couldsXPos=0;
 const int sunRadius=3;
 bool sunOrMoon = false;
 const int moonShadow=2;
 float sunXPos=-2*sunRadius;
-//clouds
+
+// clouds
 const int cloud1Width=32;
 float cloud1XPos=display.width()+cloud1Width;
+
+// notification
+int notificationBlink=0;
 
 void setupGraphics() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -34,21 +40,21 @@ void resetDisplay() {
 }
 
 void drawSunMoon() {
-  sunXPos+=0.1;
-  if(sunXPos>display.width()+2*sunRadius){
-    sunXPos=-2*sunRadius;
-    sunOrMoon=!sunOrMoon;
+  sunXPos += 0.1;
+  if(sunXPos > display.width()+2*sunRadius){
+    sunXPos = -2 * sunRadius;
+    sunOrMoon = !sunOrMoon;
   }
   
   if(sleeping){
-    sunOrMoon=true;
+    sunOrMoon = true;
   }
   
   if(!sunOrMoon){
-    display.fillCircle(sunXPos,2*sunRadius,sunRadius,WHITE);
+    display.fillCircle(sunXPos, 2 * sunRadius,sunRadius,WHITE);
   }else{
-    display.fillCircle(sunXPos,2*sunRadius,sunRadius,WHITE);
-    display.fillCircle(sunXPos-moonShadow,2*sunRadius,sunRadius,BLACK);
+    display.fillCircle(sunXPos, 2 * sunRadius,sunRadius,WHITE);
+    display.fillCircle(sunXPos - moonShadow, 2 * sunRadius,sunRadius,BLACK);
 
     if(round(cloud1XPos) % 5 == 0){
       for(int i=0;i<6;i++){
@@ -135,8 +141,8 @@ void drawGround() {
 
 void drawPoops() {
   for(int i=0; i<3; i++){
-    if(poops[i]>0){
-      display.drawBitmap(-walkXPos+poops[i], 44, poop , 16, 6, WHITE);
+    if(poopCoordinates[i]>0){
+      display.drawBitmap(-walkXPos+poopCoordinates[i], 44, poop , 16, 6, WHITE);
     }
   }
 }
@@ -151,114 +157,16 @@ void drawTrees() {
   display.drawBitmap(-treesXPos, 23, trees , 112, 20, WHITE);
 }
 
-void drawMenu() {
-  display.fillRect(0,0,display.width(),30,BLACK);
-  display.drawRect(0,0,display.width(),29,WHITE);
-  display.fillRect(1,1,display.width()-2,27,BLACK);
-  display.drawRect(0,0,display.width(),12,WHITE);
-  display.setCursor(8,2);
-  display.setTextSize(1);
-
-  if(menuDepth){
-    display.fillRect(0,0,display.width(),12,WHITE);
-    display.fillRect(1,18,1,5,WHITE);
-    display.fillRect(2,19,1,3,WHITE);
-    display.fillRect(3,20,1,1,WHITE);
-    display.setTextColor(BLACK,WHITE);
-  }else{
-    display.fillRect(1,3,1,5,WHITE);
-    display.fillRect(2,4,1,3,WHITE);
-    display.fillRect(3,5,1,1,WHITE);
-    display.setTextColor(WHITE);
-  }
-
-  char oneItem [STRING_SIZE];
-  memcpy_P (&oneItem, &mainMenu[menu][0], sizeof oneItem);
-
-  display.println(oneItem);
-  if(subMenu){
-    display.setTextColor(WHITE);
-    display.setCursor(8,16);
-    char subItem [STRING_SIZE];
-    memcpy_P (&subItem, &mainMenu[menu][subMenu], sizeof subItem);
-    display.println(subItem);
-  }
-
-  //display settings
-  if(setting > 0){
-    display.setCursor(8,16);
-    if(setting==201){
-      display.println(F("increase happiness"));
-    }
-    if(setting==301){
-      display.println(F("get some rest"));
-    }
-    if(setting==401){
-      display.println(F("keep it healthy"));
-    }
-    if(setting==501){
-      display.println(F("when health is bad"));
-    }
-    if(setting==601){
-      display.println(F("get smarter"));
-    }
-    if(setting==701 || setting==702 || setting==703 || setting==704){
-      display.drawRect(70,17,52,7,WHITE);
-    }
-    if(setting==701){
-      drawBar(hunger);
-    }
-    if(setting==702){
-      drawBar(happiness);
-    }
-    if(setting==703){
-      drawBar(health);
-    }
-    if(setting==704){
-      drawBar(discipline);
-    }
-    if(setting==705 || setting==706 || setting==801){
-      display.setCursor(80,16);
-    }
-    if(setting==705){
-      //display.setCursor(80,16);
-      display.print(weight,1);
-      display.println(F(" t"));
-    }
-    if(setting==706){
-      display.print(age,1);
-      display.println(F(" y."));
-    }  
-  }
-}
-
 void drawEatAnimation() {
   display.fillRect(0,0,display.width(),display.height(),BLACK);
   for(int j=0;j<3;j++){
     for(int i=0; i<4; i++){
       display.clearDisplay();
-      switch(action){
-        case 101:
-          //apple
-          display.drawBitmap(50,40,apple,24,24,WHITE);
-          if(j>0) display.fillCircle(76,54,12,BLACK);
-          if(j==2) display.fillCircle(47,55,12,BLACK);
-          break;
-        case 102:
-          //steak
-          display.drawBitmap(50,40,steak,24,24,WHITE);
-          if(j>0) display.fillCircle(76,59,13,BLACK);
-          if(j==2) display.fillCircle(60,63,13,BLACK);
-          break;
-        case 103:
-          //water ripples
-          display.drawCircle(80,55,1+1*i,WHITE);
-          display.drawCircle(80,55,5+2*i,WHITE);
-          display.drawCircle(80,55,10+4*i,WHITE);
-          break;
-        
 
-      }
+      display.drawBitmap(50,40,steak,24,24,WHITE);
+      if(j>0) display.fillCircle(76,59,13,BLACK);
+      if(j==2) display.fillCircle(60,63,13,BLACK);
+
       display.drawBitmap(80,24,eating[i],48,40,WHITE);
       delay(150);
       display.display();
@@ -275,15 +183,6 @@ void drawDoctorAnimation() {
     }
     display.display();
     delay(300);
-  }
-}
-
-void drawWarnAnimation() {
-  for(int i=0;i<5;i++){
-    display.setCursor(100+3*i,32);
-    display.print(F("!"));
-    display.display();
-    delay(150);
   }
 }
 
@@ -313,10 +212,6 @@ void drawDead() {
   display.setCursor(0,0);
   display.setTextColor(WHITE);
   display.println(F("Pet died...\n\nPress button 1\nto restart"));
-}
-
-void drawBar(float value){
-  display.fillRect(72,19,48*value/100,3,WHITE);
 }
 
 void show() {
